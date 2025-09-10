e91b30fa421d32a80e73693d268c0dd4814b59ee
Marians <vazquezmariana899@gmail.com>
2025-09-09 19:11:20 -0600

    Agregar funcion elige nombre

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-
 function agregarAmigo() {
     let nombreUsuario = document.getElementById("amigo").value;
     if (nombreUsuario ==""){
@@ -31,3 +30,13 @@ function limpiarCaja (){
     let ValorCaja = document.getElementById("amigo").value ="";
 }
 
+function sortearAmigo(){
+    if (amigos.length == 0) {
+        alert("No seas tramposo, agrega un nombre")
+    } else {
+        let numeroElegido = Math.floor(Math.random()*amigos.length);
+        document.getElementById("listaAmigos").innerHTML = "";
+        document.getElementById("resultado").innerHTML = (`El nombre elegido es ${amigos[numeroElegido]}`);
+        console.log(numeroElegido);
+    }
+}
